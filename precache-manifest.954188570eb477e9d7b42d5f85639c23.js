68965f1d8b44ef60fd4bc06d0e7306ceeb4325a1
unknown <luonchomotdieu@gmail.com>
2020-09-30 10:20:54 +0700

    Updates

---
+++ precache-manifest.954188570eb477e9d7b42d5f85639c23.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d58890a2f5e83d9f508",
+    "url": "/blog/static/js/main.60d11098.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "ec36b2e70516e19c1897",
+    "url": "/blog/static/js/2.f84de539.chunk.js"
+  },
+  {
+    "revision": "a580c23fcf8cc19a3ce592ab4c91eeb0",
+    "url": "/blog/index.html"
+  }
+];
